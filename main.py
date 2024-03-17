1a73e8a5cd914918424a92ff45341db82ad19a1a
yassine achehboune <44009319+SakoDev@users.noreply.github.com>
2024-03-17 22:21:39 +0800

    renamed:    img_jpg/1.jpg -> images/1.jpg 	modified:   main.py 	images/2.jpg 	images/3.jpg

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
+import os
 from rembg import remove
 from PIL import Image
 from glob import glob
 from tqdm import tqdm
 
-file_type = input("Enter the file type you want to process (jpeg/png/jpg): ").lower()
+folder_path = 'images'
 
-if file_type not in ['jpeg', 'png', 'jpg']:
-    print("Invalid file type. Please enter either 'jpeg', 'png', or 'jpg'.")
-    exit()
+image_files = glob(os.path.join(folder_path, "*"))
 
-if file_type == 'jpeg':
-    files = glob("img_jpeg/*.jpeg")
-elif file_type == 'png':
-    files = glob("img_png/*.png")
-else:  
-    files = glob("img_jpg/*.jpg")
+if not os.path.exists('dist'):
+    os.makedirs('dist')
 
-for file in tqdm(files, desc="Processing Remove Background"):
-    input_path = file
-    output_path = file.replace("img_" + file_type, "dist")
-    output_path = output_path.replace(file_type, "png")
-    input_image = Image.open(input_path)
-    output_image = remove(input_image, alpha_matting=True, alpha_matting_foreground_threshold=270,alpha_matting_background_threshold=20, alpha_matting_erode_size=11)
+for file in tqdm(image_files, desc="Processing files"):
+    file_type = file.split(".")[-1].lower()
+    if file_type not in ['jpeg', 'png', 'jpg']:
+        print(f"Ignoring file '{file}' as it is not a supported image format.")
+        continue
+
+    input_image = Image.open(file)
+    output_path = os.path.join('dist', f"{os.path.splitext(os.path.basename(file))[0]}.png")
+    output_image = remove(input_image)
     output_image.save(output_path)
