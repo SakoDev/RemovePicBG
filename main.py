ccd04ce4da5e66f947bc3bdf22601d267980fa34
yassine achehboune <44009319+SakoDev@users.noreply.github.com>
2024-03-18 14:32:05 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -11,13 +11,27 @@ image_files = glob(os.path.join(folder_path, "*"))
 if not os.path.exists('dist'):
     os.makedirs('dist')
 
+
+passed_folder = folder_path + "-passed"
+if not os.path.exists(passed_folder):
+    os.makedirs(passed_folder)
+
 for file in tqdm(image_files, desc="Processing files"):
+    if "-passed" in file:
+        print(f"Ignoring file '{file}' as it has already been processed.")
+        continue
+
     file_type = file.split(".")[-1].lower()
     if file_type not in ['jpeg', 'png', 'jpg']:
         print(f"Ignoring file '{file}' as it is not a supported image format.")
         continue
 
     input_image = Image.open(file)
-    output_path = os.path.join('dist', f"{os.path.splitext(os.path.basename(file))[0]}.png")
+    output_path = os.path.join(
+        'dist', f"{os.path.splitext(os.path.basename(file))[0]}.png")
     output_image = remove(input_image)
     output_image.save(output_path)
+
+    
+    passed_file = os.path.join(passed_folder, os.path.basename(file))
+    os.rename(file, passed_file)
